a2226e8220434951676ab06c0b7230c8d6064788
abdel2000-dply <abno870@gmail.com>
2023-06-24 23:29:26 +0100

    task 3

--- pop.c
+++ pop.c
@@ -17,9 +17,10 @@ void pop(stack_t **stack, unsigned int line_n)
 
 	temp = *stack;
 	*stack = (*stack)->next;
-	free(temp);
 
 	if (*stack != NULL)
 		(*stack)->prev = NULL;
+
+	free(temp);
 }
 
--- test.m
+++ test.m
@@ -1,7 +1,7 @@
 push 1
-pint
 push 2
-pint
 push 3
 pint
+swap
+pop
 pall
