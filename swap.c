ab48e19cd52138c586c2ce95931adebe616c479c
abdel2000-dply <abno870@gmail.com>
2023-06-24 23:33:00 +0100

    task 3

--- swap.c
+++ swap.c
@@ -7,20 +7,14 @@
  */
 void swap(stack_t **stack, unsigned int line_n)
 {
-	stack_t *temp;
+	int temp;
 
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%d: can't swap, stack too short\n", line_n);
 		exit(EXIT_FAILURE);
 	}
-
-	temp = (*stack)->next;
-	(*stack)->next = temp->next;
-	if (temp->next != NULL)
-		temp->next->prev = *stack;
-	temp->prev = NULL;
-	temp->next = *stack;
-	(*stack)->prev = temp;
-	*stack = temp;
+	temp = (*stack)->n;
+	(*stack)->n = (*stack)->next->n;
+	(*stack)->next->n = temp;
 }
