02bb453fc36c02dc2bb99b96de7c3abc3e3e0c88
abdel2000-dply <abno870@gmail.com>
2023-06-25 17:03:53 +0100

    task rotl

--- rotl.c
+++ rotl.c
@@ -7,22 +7,19 @@
  */
 void rotl(stack_t **stack, unsigned int line_n)
 {
-	stack_t *last, *temp;
+	stack_t *last;
 	(void)line_n;
 
 	if (*stack == NULL || (*stack)->next == NULL)
 		return;
 
 	last = *stack;
-	temp = last->next;
 	while (last->next != NULL)
-	{
 		last = last->next;
-		temp = last->next;
-	}
+
 	last->next = *stack;
 	(*stack)->prev = last;
-	(*stack)->next = NULL;
-	*stack = temp;
+	*stack = (*stack)->next;
+	(*stack)->prev->next = NULL;
 	(*stack)->prev = NULL;
 }
